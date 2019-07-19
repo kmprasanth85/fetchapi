e647756e75038d7045991794827036db7271f288
prasanth <prasanth@prasanth>
2019-07-19 14:26:45 +0530

    new

--- macaddfetch.py
+++ macaddfetch.py
@@ -1,7 +1,8 @@
 import requests # extra pacakge to install to run this code 
 import json
 mac = raw_input("Enter the MAC address: ") # To enter the mac address
-data = requests.get("https://api.macaddress.io/v1?apiKey=at_AUdP6j6DdX7I2i0RfWjW0WUrVdmso&output=json&search="+mac) #To fetch all the data retrived using mac address and apikey  
+apikey = raw_input("Enter the API key: ") # Enter API Key
+data = requests.get("https://api.macaddress.io/v1?apiKey="+apikey+"&output=json&search="+mac) #To fetch all the data retrived using mac address and apikey
 
 #print(data.text)
 
